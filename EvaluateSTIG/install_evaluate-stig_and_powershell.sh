9e1376da616a021becaea3d6c3887ddf40148759
Sam Insanali <sameer.insanali@gmail.com>
2025-02-18 06:49:58 -0700

    fixing bad code

--- EvaluateSTIG/install_evaluate-stig_and_powershell.sh
+++ EvaluateSTIG/install_evaluate-stig_and_powershell.sh
@@ -1,3 +1,18 @@
+###########################################################################################################################################
+###########################################################################################################################################
+
+# Created by Sam Insanali
+# GitHub: https://github.com/SInsanali
+###########################################################################################################################################
+###########################################################################################################################################
+
+# Description:
+# This script automates the installation of Evaluate-STIG and PowerShell, then checks the system for the required dependencies to run.
+
+###########################################################################################################################################
+###########################################################################################################################################
+
+
 #!/bin/bash
 
 # Define colors for output
@@ -15,21 +30,20 @@ chmod +x /opt/powershell/pwsh
 # Create a symbolic link to the PowerShell executable
 ln -sf /opt/powershell/pwsh /usr/bin/pwsh
 
-# Display user-friendly message
-echo -e "${GREEN}[ ✔ ] Symbolic link created for PowerShell.${RESET}"
-echo -e "${GREEN}[ + ] You can now execute PowerShell using: 'pwsh'${RESET}"
+#echo -e "${GREEN}[ ✔ ] Symbolic link created for PowerShell.${RESET}"
 
 # Verify PowerShell installation
 if command -v pwsh &>/dev/null; then
-    echo -e "${GREEN}[ ✔ ] PowerShell installed successfully!${RESET}"
-    echo -e "${GREEN}[ + ] PowerShell version: $(pwsh --version)${RESET}"
+    echo -e "\n${GREEN}[ ✔ ] PowerShell installed successfully!${RESET}"
+    echo -e "\n${GREEN}[ + ] PowerShell version: $(pwsh --version)${RESET}"
+    echo -e "\n${GREEN}[ + ] You can now execute PowerShell using: 'pwsh'${RESET}"
 else
     echo -e "${RED}[ ✖ ] PowerShell installation failed!${RESET}"
     exit 1
 fi
 
 # Install Evaluate-STIG
-echo -e "${GREEN}[ + ] Installing Evaluate-STIG...${RESET}"
+echo -e "\n${GREEN}[ + ] Installing Evaluate-STIG...${RESET}"
 
 mkdir -p /opt/Evaluate-STIG
 unzip -q ./Evaluate-STIG*.zip -d /opt/Evaluate-STIG  # Quiet extraction
@@ -45,7 +59,7 @@ echo -e "${GREEN}[ + ] Testing prerequisites...${RESET}"
 /opt/Evaluate-STIG/Evaluate-STIG/Prerequisites/Test-Prerequisites.sh
 
 # Check for required packages
-echo -e "${GREEN}[ + ] Checking for required packages...${RESET}"
+echo -e "\n${GREEN}[ + ] Checking for required packages...${RESET}"
 
 missing_packages=()
 for package in libicu lshw; do
@@ -56,8 +70,8 @@ done
 
 # If missing packages were found, display an alert
 if [ ${#missing_packages[@]} -ne 0 ]; then
-    echo -e "${RED}[ ! ] The following packages are missing: ${missing_packages[*]}${RESET}"
+    echo -e "\n${RED}[ ! ] The following packages are missing: ${missing_packages[*]}${RESET}"
     echo -e "${RED}[ ! ] Run eval_stig_dependency_check.sh to install them.${RESET}"
 else
-    echo -e "${GREEN}[ ✔ ] All required packages are installed.${RESET}"
+    echo -e "\n${GREEN}[ ✔ ] All required packages are installed.${RESET}"
 fi
